7ec5e2f4c0b92a22ad937ed17cf1d1692c4c92b6
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-08-03 09:19:29 -0700

    js challenge #3

--- starter/script.js
+++ starter/script.js
@@ -75,3 +75,76 @@ if (money) {
 } else {
     console.log("you should get a job!")
 }
+
+// Equality operators
+
+// const favourite = prompt("what is your favourite number?");
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite == 23) {
+//     console.log("Cool! 23 is an amazing number!");
+// }
+
+// Boolean Logic - AND, OR and NOT operators
+// Logical Operators
+
+const hasDriversLicense = false; // A
+const hasGoodVision = false; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const isTired = true; // C
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!')
+} else {
+    console.log('Someone else should drive.....')
+}
+
+// Coding Challenge #3
+
+// Dolphin scores
+
+const dolphinScore1 = 96;
+const dolphinScore2 = 108;
+const dolphinScore3 = 89;
+
+// Koala scores
+
+const koalaScore1 = 99;
+const koalaScore2 = 108;
+const koalaScore3 = 89;
+
+const dolphinAvg = (dolphinScore1 + dolphinScore2 + dolphinScore3)/3;
+const koalaAvg = (koalaScore1 + koalaScore2 + koalaScore3)/3;
+
+// check which team has more points
+if (dolphinAvg > koalaAvg) {
+    console.log(`Dolphin Average is higher with ${dolphinAvg} points!`)
+} else if (koalaAvg > dolphinAvg) {
+    console.log(`Koala Average is higher with ${koalaAvg} points`)
+} else {
+    console.log(`Teams tied with ${dolphinAvg} points`)
+}
+
+// check if teams have over 100 points
+
+if (dolphinAvg > 100) {
+    console.log(`Yay the Dolphin's got over 100 points.
+    Points: ${dolphinAvg}`)
+} else {
+    console.log(`Oops, the Dolphin's didn't make it, the need more points.
+    Points: ${dolphinAvg}`)
+}
+
+if (koalaAvg > 100) {
+    console.log(`Yay the Koala's got over 100 points.
+    Points: ${koalaAvg}`)
+} else {
+    console.log(`Oops, the Koala's didn't make it, they need more points.
+    Points: ${koalaAvg}`)
+}
+
