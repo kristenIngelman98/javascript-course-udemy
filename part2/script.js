795ac1968b2de3bbd5c3250a44a9d07d3306db4c
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-09-17 06:56:30 -0700

    arrow function example

--- part2/script.js
+++ part2/script.js
@@ -44,3 +44,18 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+
+// Arrow Function (shorter and faster to write) ES6
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1998, 'Kristen'));
