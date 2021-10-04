4dc18744f33ec1db3256f31285935b9666a1e197
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-04 11:31:29 -0700

    created number guess logic

--- guess-my-number/starter/script.js
+++ guess-my-number/starter/script.js
@@ -1,19 +1,34 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🥳 Correct Number!';
+// document.querySelector('.message').textContent = '🥳 Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10; 
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10; 
 
-// get value from query selector
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// // get value from query selector
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 // handling click events
 document.querySelector('.check').addEventListener('click', function() {
-    const guess = document.querySelector('.guess').value;
-    console.log(guess);
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '🚫 No number!'
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🥳 Correct Number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too High!!';
+    } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉  Too Low!!';
+}
 });
+
+
 
