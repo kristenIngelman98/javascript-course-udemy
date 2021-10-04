f7a7a3dfc83ad76ca2069dc136224b3a690da4eb
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-04 11:12:47 -0700

    DOM manipulation review

--- guess-my-number/starter/script.js
+++ guess-my-number/starter/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🥳 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10; 
+
+// get value from query selector
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
