bfee251c8263f041df34c62139671ac999dcc482
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-04 11:18:12 -0700

    addEventListener example

--- guess-my-number/starter/script.js
+++ guess-my-number/starter/script.js
@@ -11,3 +11,9 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 
+// handling click events
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = document.querySelector('.guess').value;
+    console.log(guess);
+});
+
