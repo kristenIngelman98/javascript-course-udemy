e1c634257a2bfc37f666489c99c429eda1fd9a71
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-04 12:33:16 -0700

    added restore button functionality

--- guess-my-number/starter/script.js
+++ guess-my-number/starter/script.js
@@ -11,8 +11,7 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
@@ -32,6 +31,7 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor = '#60b347';
     
         document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
     }
     // When guess is too high
     else if (guess > secretNumber) {
@@ -57,6 +57,19 @@ document.querySelector('.check').addEventListener('click', function() {
         }
     }
 });
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score; 
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+   
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
 
 
 
