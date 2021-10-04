cf240207ba22e3dd933f46b6d05bf9d9572f3903
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-04 12:11:34 -0700

    updated game logic

--- guess-my-number/starter/script.js
+++ guess-my-number/starter/script.js
@@ -14,6 +14,8 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 // handling click events
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -24,10 +26,26 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🥳 Correct Number!';
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = '📈 Too High!!';
+        if (score > 0) {
+            document.querySelector('.message').textContent = '📈 Too High!!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+
     } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉  Too Low!!';
-}
+        if (score > 0) {
+            document.querySelector('.message').textContent = '📉  Too Low!!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+
+    }
 });
 
 
