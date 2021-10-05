9b5ddd8cab7bdb4031003b5a91ee1ae4c35374a3
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-05 09:12:05 -0700

    enabled modal and overlay

--- modal/starter/script.js
+++ modal/starter/script.js
@@ -6,6 +6,12 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
+// Get textContent of all .show-modal class
 for (let i = 0; i < btnOpenModal.length; i++) {
-    console.log(btnOpenModal[i].textContent);
+    btnOpenModal[i].addEventListener('click', function() {
+        console.log('Button Clicked')
+        // removing hidden class from .modal & .overlay classes to show the modal when clicked
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
 }
