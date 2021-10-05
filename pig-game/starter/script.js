9434d160e629eda4aef5c91b3c7951d5d4cc2f83
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-05 11:14:37 -0700

    finished pig-game

--- pig-game/starter/script.js
+++ pig-game/starter/script.js
@@ -13,32 +13,54 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// variables live out here but we reassign them a value insite the init() function
+let scores, currentScore, activePlayer, playing;
 
-score0El.textContent = 0;
-score1El.textContent = 0;
+// Starting conditions
+const init = function() {
+    // only available inside the function (not outside)
+    scores = [0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
 
-// Add hidden class to .dice class (hide dice in browser)
-diceEl.classList.add('hidden');
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    // Add hidden class to .dice class (hide dice in browser)
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0; // set current score back to 0
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    
+    // toggle method: if class is not there it will add player-active
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function() {
+    if (playing) {
+    
     // 1. Generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1; //give us a number between 0 and 6
-
-
+    
     // 2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
-    // console.log(dice);
-
-
 
     // 3. Check for rolled 1: if true, switch to next player
-
     if (dice !== 1) {
         // add dice to current score
         currentScore += dice;
@@ -46,18 +68,34 @@ btnRoll.addEventListener('click', function() {
     } 
     else {
         // Switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0; // set current score back to 0
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        // toggle method: if class is not there it will add player-active
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-
-
+        switchPlayer();
     }
-
+    }
 })
 
 
 
+btnHold.addEventListener('click', function() {
+    if (playing) {
+    // 1. add current score to active player's score
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
+    // scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    // 2. check is player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+        // Finish the game
+        playing = false;
+        // add back on the hidden class to hide the dice when game finishes
+        diceEl.classList.add('hidden');
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+    } 
+    else {
+        // 3. switch to the next player
+        switchPlayer();
+    }
+    }
+})
 
+btnNew.addEventListener('click', init);
