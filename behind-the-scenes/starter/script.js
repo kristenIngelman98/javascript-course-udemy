be28d7bf856e7617392279552bee4109f359cebf
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-10-07 12:45:04 -0700

    scoping examples

--- behind-the-scenes/starter/script.js
+++ behind-the-scenes/starter/script.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function calcAge(birthYear) {
-    const age = 2037 - birthYear;
-    console.log(firstName)
+    const age = 2021 - birthYear;
+    
+    function printAge() {
+        const output = `${firstName} is ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            const str = `Oh, you are a millenial, ${firstName}`
+            console.log(str);
+        }
+    }
+
+    printAge();
     return age;
 }
 
 const firstName = 'Kristen';
-
-calcAge(1998);
+calcAge(1995);
